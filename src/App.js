1dc8d7dd21d31639b0eac0752b0ae5e655d644f6
Intizar Ahmad <intizar.ahmad@telus.com>
2019-10-13 18:38:39 +0530

    readme updated

--- src/App.js
+++ src/App.js
@@ -2,15 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import PlayerPanel from './player/PlayerPanel'
 import LABELS from './constants'
-/*
-GAME RULES:
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
 
-*/
 
 function App() {
   const initialPlayersState = [
